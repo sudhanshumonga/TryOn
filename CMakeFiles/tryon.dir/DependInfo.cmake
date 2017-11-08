353a64126c924ed1c408c1faec029b8fa84152a8
Sudhanshu Monga <iit2014081@iiita.ac.in5>
2017-11-08 22:46:02 +0530

    uploading entire project

---
+++ CMakeFiles/tryon.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sudhanshu/Projects/TryOn/tryon.cpp" "/home/sudhanshu/Projects/TryOn/CMakeFiles/tryon.dir/tryon.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/sudhanshu/Projects/TryOn/dlib_build/CMakeFiles/dlib.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "lib/dlib/dlib/.."
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ CMakeFiles/tryon.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tryon.dir/tryon.cpp.o"
+  "bin/tryon.pdb"
+  "bin/tryon"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tryon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
